953fbf1c1ebd907622024ec004ac848c78dba1fd
santiago0598 <santiagosv300@gmail.com>
2024-08-18 21:37:27 -0500

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -213,24 +213,24 @@ function ProductList() {
         }
     ];
    const styleObj={
-    backgroundColor: #4CAF50,
-    color: #fff!important,
-    padding: 15px,
-    display: flex,
-    justifyContent: space-between,
-    alignIems: center,
-    fontSize: 20px,
+    backgroundColor: '#4CAF50',
+    color: '#fff!important',
+    padding: '15px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignIems: 'center',
+    fontSize: '20px',
    }
    const styleObjUl={
-    display: flex,
-    justifyContent: space-between,
-    alignItems: center,
-    width: 1100px,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '1100px',
    }
    const styleA={
-    color: white,
-    fontSize: 30px,
-    textDecoration: none,
+    color: 'white',
+    fontSize: '30px',
+    textDecoration: 'none',
    }
    const handleCartClick = (e) => {
     e.preventDefault();
